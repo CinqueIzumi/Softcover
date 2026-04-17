cb1a51bd7e03460590f5e9df600ab7110922b323
Cinque <ba.peereboom@gmail.com>
2026-04-17 17:22:45 +0200

    Bump version to 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "nl.rhaydus.softcover"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.2.1"
+        versionCode = 18
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
