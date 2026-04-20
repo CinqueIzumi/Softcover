4568bc64ec6df4e66c5d3c3fcf6347bbfa139de8
Cinque <ba.peereboom@gmail.com>
2026-04-20 20:12:50 +0200

    Bump version name & code for 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "nl.rhaydus.softcover"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.2.2"
+        versionCode = 19
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
