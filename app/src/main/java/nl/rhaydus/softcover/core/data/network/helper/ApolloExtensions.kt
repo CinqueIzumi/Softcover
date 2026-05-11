a1b3a966bff4101e2b5095aa76544edb1dff2420
Cinque <ba.peereboom@gmail.com>
2026-05-11 18:35:00 +0200

    Make safeQuery policy-aware and add safeQueryFlow for cache-first reads

--- app/src/main/java/nl/rhaydus/softcover/core/data/network/helper/ApolloExtensions.kt
+++ app/src/main/java/nl/rhaydus/softcover/core/data/network/helper/ApolloExtensions.kt
@@ -5,19 +5,21 @@ import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.api.Mutation
 import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.api.Query
+import com.apollographql.apollo.cache.normalized.FetchPolicy
+import com.apollographql.apollo.cache.normalized.fetchPolicy
+import com.apollographql.apollo.exception.ApolloNetworkException
+import com.apollographql.apollo.exception.CacheMissException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import nl.rhaydus.softcover.core.domain.connectivity.NetworkAvailability
 import nl.rhaydus.softcover.core.domain.exception.OfflineException
 
-private suspend fun <T : Operation.Data> executeCall(
-    call: suspend () -> ApolloResponse<T>,
-): T {
-    if (NetworkAvailability.isOnline().not()) {
-        throw OfflineException()
-    }
-
-    val response = call()
-
+private fun <T : Operation.Data> requireData(response: ApolloResponse<T>): T {
     response.exception?.let { exception ->
+        if (exception is ApolloNetworkException && NetworkAvailability.isOnline().not()) {
+            throw OfflineException()
+        }
+
         throw RuntimeException(
             "Apollo network error: ${exception.message}",
             exception,
@@ -43,8 +45,56 @@ private suspend fun <T : Operation.Data> executeCall(
     return response.data ?: throw RuntimeException("Apollo response had no data and no errors")
 }
 
-suspend fun <T : Mutation.Data> ApolloClient.safeMutation(mutation: Mutation<T>): T =
-    executeCall { this.mutation(mutation).execute() }
+private suspend fun <T : Operation.Data> executeCall(
+    requireNetwork: Boolean,
+    call: suspend () -> ApolloResponse<T>,
+): T {
+    if (requireNetwork && NetworkAvailability.isOnline().not()) {
+        throw OfflineException()
+    }
 
-suspend fun <T : Query.Data> ApolloClient.safeQuery(query: Query<T>): T =
-    executeCall { this.query(query).execute() }
+    return requireData(call())
+}
+
+suspend fun <T : Mutation.Data> ApolloClient.safeMutation(mutation: Mutation<T>): T =
+    executeCall(requireNetwork = true) { this.mutation(mutation).execute() }
+
+suspend fun <T : Query.Data> ApolloClient.safeQuery(
+    query: Query<T>,
+    fetchPolicy: FetchPolicy = FetchPolicy.NetworkOnly,
+): T = executeCall(requireNetwork = fetchPolicy == FetchPolicy.NetworkOnly) {
+    this.query(query).fetchPolicy(fetchPolicy).execute()
+}
+
+fun <T : Query.Data> ApolloClient.safeQueryFlow(
+    query: Query<T>,
+    fetchPolicy: FetchPolicy = FetchPolicy.CacheAndNetwork,
+): Flow<T> = flow {
+    var lastFailure: Throwable? = null
+    var emittedAny = false
+
+    this@safeQueryFlow.query(query).fetchPolicy(fetchPolicy).toFlow().collect { response ->
+        val data = response.data
+
+        if (data != null && response.errors.isNullOrEmpty()) {
+            emit(data)
+            emittedAny = true
+        } else {
+            response.exception?.let { exception ->
+                if (exception !is CacheMissException) {
+                    lastFailure = exception
+                }
+            }
+        }
+    }
+
+    if (emittedAny.not()) {
+        val failure = lastFailure
+
+        if (failure is ApolloNetworkException && NetworkAvailability.isOnline().not()) {
+            throw OfflineException()
+        }
+
+        throw failure ?: RuntimeException("Apollo flow completed with no data")
+    }
+}
