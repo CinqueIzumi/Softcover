8e6d514cdda67354ba323b92f75be58e3e381834
Cinque <ba.peereboom@gmail.com>
2026-05-11 18:30:00 +0200

    Wire in-memory normalized cache into ApolloClient

--- app/src/main/java/nl/rhaydus/softcover/di/ApolloModule.kt
+++ app/src/main/java/nl/rhaydus/softcover/di/ApolloModule.kt
@@ -1,11 +1,15 @@
 package nl.rhaydus.softcover.di
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.api.MemoryCacheFactory
+import com.apollographql.apollo.cache.normalized.normalizedCache
 import com.apollographql.apollo.network.okHttpClient
 import nl.rhaydus.softcover.core.data.network.interceptor.AuthInterceptor
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import java.util.concurrent.TimeUnit
+
+private const val APOLLO_MEMORY_CACHE_BYTES = 10 * 1024 * 1024
 
 val apolloModule = module {
     single {
@@ -24,6 +28,7 @@ val apolloModule = module {
         ApolloClient.Builder()
             .serverUrl("https://api.hardcover.app/v1/graphql")
             .okHttpClient(get())
+            .normalizedCache(MemoryCacheFactory(maxSizeBytes = APOLLO_MEMORY_CACHE_BYTES))
             .build()
     }
 }
