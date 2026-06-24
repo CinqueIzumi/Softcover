ff4dbbcc08c4fd7b92643e3a0b23503ad5825a0e
Cinque <ba.peereboom@gmail.com>
2026-06-24 16:38:21 +0200

    Fix feature:session multiplatform build: trim androidMain-only includes from common Koin module

--- feature/session/src/commonMain/kotlin/nl/rhaydus/softcover/feature/session/di/SessionModule.kt
+++ feature/session/src/commonMain/kotlin/nl/rhaydus/softcover/feature/session/di/SessionModule.kt
@@ -1,13 +1,7 @@
 package nl.rhaydus.softcover.feature.session.di
 
 import org.koin.dsl.module
-import nl.rhaydus.softcover.core.designsystem.presentation.di.designSystemModule
-import nl.rhaydus.softcover.core.notification.di.notificationModule
 
 val sessionModule = module {
-    includes(
-        platformSessionModule,
-        designSystemModule,
-        notificationModule,
-    )
+    includes(platformSessionModule)
 }
