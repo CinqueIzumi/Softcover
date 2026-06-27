e7d65d6517c16f16154c6771cab9d9dc9b599044
Cinque <ba.peereboom@gmail.com>
2026-06-27 19:13:07 +0200

    Set version to 3.0.0 (build 30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "nl.rhaydus.softcover"
         minSdk = 26
         targetSdk = 37
-        versionCode = 29
-        versionName = "2.4.0"
+        versionCode = 30
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
